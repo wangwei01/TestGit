6416ac96c95c1058316098b38b4dd5f96faa0661
wangwei <wangwei@huami.com>
2018-04-11 15:48:22 +0800

    修改东西

--- app/src/main/java/com/example/asus/testgit/MainActivity.java
+++ app/src/main/java/com/example/asus/testgit/MainActivity.java
@@ -9,9 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-=======
         //准备提一次提交注释
->>>>>>> origin/wangweifirst
     }
 }
